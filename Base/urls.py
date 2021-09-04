a33b2aa1e8ed221647c2f6146ceb5eee5e993c68
Rohit Purty <55530094+raymondM5GT@users.noreply.github.com>
2021-09-04 18:21:40 +0530

    Add files via upload

--- Base/urls.py
+++ Base/urls.py
@@ -4,10 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
+
     path('project/<str:pk>', views.projectPage, name='project'),
     path('add-project/', views.addProject, name='addProject'),
     path('edit-project/<str:pk>', views.editProject, name='editProject'),
+
     path('inbox/', views.inboxPage, name='inbox'),
     path('message/<str:pk>', views.messagePage, name='message'),
+
+    path('add-skill/', views.addskill, name='add-skill'),
+    
+    path('add-endorsement/', views.addEndorsement, name='add-endorsement'),
+
 
 ]
